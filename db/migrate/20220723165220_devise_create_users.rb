4139ee7f758ed5a519ad9eb92fd5e495fed3cdcc
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-07-23 17:04:24 +0000

    [Update]added t.string :name on 20220723165220_devise_create_users, rails db:migrate

--- db/migrate/20220723165220_devise_create_users.rb
+++ db/migrate/20220723165220_devise_create_users.rb
@@ -32,7 +32,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      #名前を保存するカラム
+      t.string :name
       t.timestamps null: false
     end
 
